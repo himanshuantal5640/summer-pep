3f227a92ab5eb9c3e942e4d565e4cb0e925132b7
himanshu-antal <himanshuantal26@gmail.com>
2026-07-13 16:54:16 +0530

    using useContext

--- React/mini-project/src/components/navbar/Navbar.jsx
+++ React/mini-project/src/components/navbar/Navbar.jsx
@@ -1,32 +1,35 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { useTheme } from "../../context/ThemeContext";
 import "./Navbar.css";
 
 const Navbar = ({ isLoggedIn, setIsLoggedIn }) => {
   const navigate = useNavigate();
 
+  const { theme, toggleTheme } = useTheme();
+
   const handleLogout = () => {
     localStorage.removeItem("isLoggedIn");
-
     setIsLoggedIn(false);
-
     navigate("/login");
   };
 
   return (
     <nav className="navbar">
-      <h2 className="logo">TechApp</h2>
+      <h2 className="logo">CourseHub</h2>
 
       <div className="nav-links">
         <Link to="/">Home</Link>
-
         <Link to="/about">About</Link>
 
+        <button className="theme-btn" onClick={toggleTheme}>
+          <div
+            className={`toggle-circle ${theme === "dark" ? "active" : ""}`}
+          ></div>
+        </button>
+
         {isLoggedIn ? (
-          <button
-            className="logout-btn"
-            onClick={handleLogout}
-          >
+          <button className="logout-btn" onClick={handleLogout}>
             Logout
           </button>
         ) : (
